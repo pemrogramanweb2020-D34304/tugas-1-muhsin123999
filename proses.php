5c71dd3069b803cd174f9f46d0f3ede18c441c9e
Alief Muhsin Mubarok <71061633+muhsin123999@users.noreply.github.com>
2020-09-28 22:54:52 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,24 @@
+<?php
+$bilangan1 = $_GET['bilangan1'];
+$bilangan2 = $_GET['bilangan2'];
+$hitung = $_GET['hitung'];
+echo "Bilangan 1 adalah $bilangan1 <br>";
+echo "Bilangan 2 adalah $bilangan2 <br>";
 
+if ($hitung == 1) {
+	$tambah = $bilangan1 + $bilangan2;
+	echo "Hasil pertambahan adalah $tambah";
+}
+elseif ($hitung == 2) {
+	$kurang = $bilangan1 - $bilangan2;
+	echo "Hasil pengurangan adalah $kurang";
+}
+elseif ($hitung == 3) {
+	$kali = $bilangan1 * $bilangan2;
+	echo "Hasil perkalian adalah $kali";
+}
+elseif ($hitung == 4) {
+	$bagi = $bilangan1 / $bilangan2;
+	echo "Hasil pembagian adalah $bagi";
+}
+?>
